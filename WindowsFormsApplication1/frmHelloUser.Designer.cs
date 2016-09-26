c2d020dba6ffc8280ddb0d89e8b59317104b8cef
Dylan Angell <d.angell@sjasd.ca>
2016-09-26 10:37:46 -0500

    complete first question
    finished product

---
+++ WindowsFormsApplication1/frmHelloUser.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class frmAssignmentOne
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnWorld = new System.Windows.Forms.Button();
+            this.lblWorldbtn = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnWorld
+            // 
+            this.btnWorld.Location = new System.Drawing.Point(12, 12);
+            this.btnWorld.Name = "btnWorld";
+            this.btnWorld.Size = new System.Drawing.Size(260, 23);
+            this.btnWorld.TabIndex = 0;
+            this.btnWorld.Text = "Arnold????";
+            this.btnWorld.UseVisualStyleBackColor = true;
+            this.btnWorld.Click += new System.EventHandler(this.btnWorld_Click);
+            // 
+            // lblWorldbtn
+            // 
+            this.lblWorldbtn.AutoSize = true;
+            this.lblWorldbtn.Location = new System.Drawing.Point(34, 73);
+            this.lblWorldbtn.Name = "lblWorldbtn";
+            this.lblWorldbtn.Size = new System.Drawing.Size(0, 13);
+            this.lblWorldbtn.TabIndex = 1;
+            // 
+            // frmAssignmentOne
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblWorldbtn);
+            this.Controls.Add(this.btnWorld);
+            this.Name = "frmAssignmentOne";
+            this.Text = "Hello World";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnWorld;
+        private System.Windows.Forms.Label lblWorldbtn;
+    }
+}
+
---
+++ WindowsFormsApplication1/frmHelloUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class frmAssignmentOne : Form
+    {
+        public frmAssignmentOne()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWorld_Click(object sender, EventArgs e)
+        {
+            lblWorldbtn.Text = "GET 2 DA CHOPPA";
+        }
+    }
+}
